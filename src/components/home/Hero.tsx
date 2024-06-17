c4f93e34b756afc62e2d5c203ae46f3e73fc5327
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-06-17 15:09:28 +0200

    Hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,36 +1,94 @@
+"use client";
+import { gsap } from "gsap";
 import { ArrowDown } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Hero: React.FC = () => {
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to(
+      ".hero_title",
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.05,
+        duration: 0.5,
+        ease: "power2.out",
+      },
+      0
+    )
+      .to(
+        ".hero_description",
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "+=0.3"
+      )
+      .to(
+        ".hero_button",
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "+=0.3"
+      )
+      .to(
+        ".hero_subtitle",
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "+=0.3"
+      )
+      .to(
+        ".right_text",
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.5,
+          ease: "power2.out",
+        },
+        "+=0.3"
+      );
+  }, []);
+
   return (
     <div className="pt-28 flex justify-between">
       <div>
-        <div className="px-10 ">
+        <div className="px-10">
           <div className="flex flex-col gap-16">
             <div>
-              <h1 className="text-9xl font-bold">
+              <h1 className="hero_title text-9xl font-bold opacity-0 translate-y-10">
                 Unleash Your <br />
                 Energy
               </h1>
             </div>
-            <div className="pl-1">
+            <div className="hero_description pl-1 opacity-0 translate-y-10">
               <p className="text-3xl font-extralight">
                 Experience the Unmatched Power and <br />
                 Endurance of Our Revolutionary Energy Drink
               </p>
             </div>
-            <div>
+            <div className="hero_button opacity-0 translate-y-10">
               <button className="py-5 px-10 rounded-full bg-black transition text-white font-thin text-xl hover:bg-white hover:text-black">
                 Buy Now
               </button>
             </div>
           </div>
         </div>
-        <div className="bg-[#90FF22] w-fit p-[0.5em] mt-28">
+        <div className="hero_subtitle bg-[#90FF22] w-fit p-[0.5em] mt-28 opacity-0 translate-y-10">
           <h1 className="left-[5em] relative text-xl">The best of flavors</h1>
         </div>
       </div>
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col justify-between right_text opacity-0 translate-y-10">
         <div className="bg-[#e8e8e8] p-5">
           <div>
             <h1 className="text-3xl">Incredibly Refreshing</h1>
