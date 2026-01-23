25fc31599d8cf4df96bf4e519b02b3d0229fcd51
Dzenis <dzenisgudzevic18@gmail.com>
2026-01-23 13:06:44 +0100

    fix: Correct rotation values for DRINK_POSES to improve animation accuracy

--- src/components/canvas/animations/poses/drink.ts
+++ src/components/canvas/animations/poses/drink.ts
@@ -4,21 +4,21 @@ export const DRINK_POSES: Pose[] = [
   {
     at: 0,
     position: { x: 2, y: 0, z: 0 },
-    rotation: { x: 0.9, y: Math.PI * 2.5, z: -1.0 },
+    rotation: { x: 0.2, y: 2.5, z: 0.5 },
   },
   {
     at: 0.25,
-    position: { x: -1, y: 0, z: 0 },
-    rotation: { x: 0.9, y: -0.3, z: -1.0 },
+    position: { x: 0, y: 0, z: 0 },
+    rotation: { x: -3.2, y: -3.2, z: 1.6 },
   },
   {
     at: 0.5,
     position: { x: 0, y: 0, z: 1 },
-    rotation: { x: 1.7, y: 0, z: 4.7 },
+    rotation: { x: -3.2, y: -3.2, z: -3.15 },
   },
   {
     at: 0.75,
     position: { x: 0, y: 0, z: 1.5 },
-    rotation: { x: 0, y: -4.7, z: -0.1 },
+    rotation: { x: 0, y: -3.2, z: 0 },
   },
 ];
