912dcff4c794e38e39002b7fec6805ece36cb62d
Dzenis <dzenisgudzevic18@gmail.com>
2026-01-23 16:17:55 +0100

    fix: Update positions for strawberry poses to improve animation alignment

--- src/components/canvas/animations/poses/strawberries.ts
+++ src/components/canvas/animations/poses/strawberries.ts
@@ -3,7 +3,7 @@ import { Pose } from "../types";
 export const STRAWBERRY_LEFT_POSES: Pose[] = [
   {
     at: 0,
-    position: { x: -4, y: -2, z: -2 },
+    position: { x: 0.2, y: -1, z: -2 },
     rotation: { x: 0, y: 0, z: 0 },
     scale: 1,
   },
@@ -48,7 +48,7 @@ export const STRAWBERRY_RIGHT_POSES: Pose[] = [
   },
   {
     at: 0.75,
-    position: { x: 4, y: -2, z: -2 },
+    position: { x: 4, y: -1, z: -2 },
     rotation: { x: 0, y: 0, z: 2 },
     scale: 0.8,
   },
@@ -57,26 +57,26 @@ export const STRAWBERRY_RIGHT_POSES: Pose[] = [
 export const STRAWBERRY_TOP_POSES: Pose[] = [
   {
     at: 0,
-    position: { x: 0, y: 3, z: -3 },
+    position: { x: 0.5, y: 1, z: -3 },
     rotation: { x: 0, y: 0, z: Math.PI },
     scale: 0.5,
   },
   {
     at: 0.25,
-    position: { x: 1, y: 4, z: -2 },
+    position: { x: 1, y: 1.5, z: -2 },
     rotation: { x: 0, y: 0, z: Math.PI + 0.5 },
     scale: 0.6,
   },
   {
     at: 0.5,
-    position: { x: -1, y: 3.5, z: -1 },
+    position: { x: -1, y: -1.5, z: -1 },
     rotation: { x: 0, y: 0, z: Math.PI + 1 },
     scale: 0.4,
   },
   {
     at: 0.75,
-    position: { x: 0, y: 3, z: -3 },
+    position: { x: 2, y: 0.4, z: -3 },
     rotation: { x: 0, y: 0, z: Math.PI + 1.5 },
-    scale: 0.5,
+    scale: 1,
   },
 ];
